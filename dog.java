5af65d28750cfd4d548cafc1258564019848bd20
RamTamang1 <122420578+RamTamang1@users.noreply.github.com>
2023-06-10 11:17:07 -0400

    modified dog

---
+++ dog.java
@@ -0,0 +1,27 @@
+package Dog;
+
+public class dog {
+	
+
+	//add attributes
+	    		String name="Tommy";
+			String color="Brown";
+			int leg=4;
+			int tail=1;
+			//create some method/behavior
+			 void run() {
+			 System.out.println("dog can run");
+			}
+	        void Sleep() {
+			System.out.println("dog can sleep");
+	        }
+	         void bark() {
+			System.out.println("Dog can bark");
+	         }
+			void walk() {
+				System.out.println("Dog can      always walk");
+			}
+			
+						}
+						
+			
